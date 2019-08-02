cb5ead1865a599200d6f5209f6e800fe2b8ad51b
Paweł Przeniczny <pawel.przeniczny@airhelp.com>
2019-08-02 14:46:58 +0200

    *.js in js dir.

--- 002.js
+++
@@ -1,5 +0,0 @@
-// create new instance of JS Date object
-let date = new Date();
-
-// see what's inside
-console.log(date);
--- 003.js
+++
@@ -1,6 +0,0 @@
-// create new instance of JS Date object
-let date = new Date();
-
-// call a method on an object
-let minutes = date.getMinutes();
-console.log(minutes);
